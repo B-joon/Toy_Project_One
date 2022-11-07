485760e0f8460652cc6cedab5c353d7f3fb85589
B-joon <dosel45@gmail.com>
2022-11-07 22:17:25 +0900

    프로젝트 기본 셋팅

---
+++ src/main/java/com/example/matbap/MatBapApplication.java
@@ -0,0 +1,13 @@
+package com.example.matbap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatBapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatBapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/matbap/MatBapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.matbap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatBapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
